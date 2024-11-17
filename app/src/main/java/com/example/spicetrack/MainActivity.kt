6e171090ed2d7ca28fab1a5384e0652a48f61536
alya131 <alsyamadaniah@gmail.com>
2024-11-17 14:11:54 +0700

    file register

--- app/src/main/java/com/example/spicetrack/MainActivity.kt
+++ app/src/main/java/com/example/spicetrack/MainActivity.kt
@@ -6,16 +6,32 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.spicetrack.databinding.ActivityMainBinding
+import com.example.spicetrack.home.ui.register.RegisterActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        // Inisialisasi View Binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Atur padding sesuai dengan insets sistem
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Set up tombol untuk memulai LoginActivity
+        binding.btnGetStarted.setOnClickListener {
+            val loginIntent = Intent(this, RegisterActivity::class.java)
+            startActivity(loginIntent)
         }
     }
 }
--- app/src/main/java/com/example/spicetrack/home/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/spicetrack/home/ui/login/LoginActivity.kt
@@ -1,12 +1,23 @@
 package com.example.spicetrack
 
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.spicetrack.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //  kembali ke MainActivity saat ikon panah diklik
+        binding.backIcon.setOnClickListener {
+            finish()
+        }
     }
 }
