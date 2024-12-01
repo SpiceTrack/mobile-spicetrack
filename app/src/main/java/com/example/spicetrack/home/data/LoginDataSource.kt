add7043d266650a88b9009fd0e0680927179dbfa
alya131 <alsyamadaniah@gmail.com>
2024-12-01 20:15:32 +0700

    new

--- app/src/main/java/com/example/spicetrack/home/data/LoginDataSource.kt
+++ app/src/main/java/com/example/spicetrack/home/data/LoginDataSource.kt
@@ -8,17 +8,17 @@ import java.io.IOException
  */
 class LoginDataSource {
 
-    fun login(username: String, password: String): Result<LoggedInUser> {
-        try {
+//    fun login(username: String, password: String): Result<LoggedInUser> {
+//        try {
+//
+//            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
+//            return Result.Success(fakeUser)
+//        } catch (e: Throwable) {
+//            return Result.Error(IOException("Error logging in", e))
+//        }
+//    }
+//
+//    fun logout() {
 
-            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
-            return Result.Success(fakeUser)
-        } catch (e: Throwable) {
-            return Result.Error(IOException("Error logging in", e))
-        }
-    }
-
-    fun logout() {
-
-    }
+//    }
 }
