df28f500d00e5e457df88709180ae70309ce0dc0
alya131 <alsyamadaniah@gmail.com>
2024-12-07 13:20:48 +0700

    ml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,16 +39,24 @@ android {
         viewBinding = true
     }
 }
+
 configurations.all {
-    resolutionStrategy.force ("org.jetbrains:annotations:23.0.0")
+    resolutionStrategy.force("org.jetbrains:annotations:23.0.0")
 }
 
 dependencies {
 
+    // CameraX dependencies
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+
+    // Room
     implementation(libs.androidx.room.runtime)
-    implementation(libs.androidx.annotation)
+    implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
 
+    // Datastore, Coroutines, and other libraries
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.jetbrains.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
@@ -57,6 +65,8 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.retrofit2.converter.gson)
     implementation(libs.logging.interceptor)
+
+    // AndroidX libraries
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -66,10 +76,10 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.activity)
+    implementation(libs.androidx.lifecycle.runtime.ktx.v262)
+
+    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.androidx.room.ktx)
-    implementation(libs.lifecycle.runtime.ktx)
-
 }
--- app/src/main/java/com/example/spicetrack/home/ui/network/Apiservice.kt
+++ app/src/main/java/com/example/spicetrack/home/ui/network/Apiservice.kt
@@ -1,56 +1,15 @@
 package com.example.spicetrack.home.ui.network
-//import androidx.room.Query
-//import retrofit2.Call
-//import retrofit2.http.Body
-//import retrofit2.http.GET
-//import retrofit2.http.POST
+
+import okhttp3.MultipartBody
+import retrofit2.http.*
 
 
 interface ApiService {
-
-//    @POST("auth/register")
-//    fun registerUser(
-//        @Body requestBody: RegisterRequest
-//    ): Call<ApiResponse>
-//
-//    @POST("auth/login")
-//    fun loginUser(
-//        @Body requestBody: LoginRequest
-//    ): Call<ApiResponse>
-//
-//    @POST("auth/logout")
-//    fun logoutUser(): Call<ApiResponse>
-//
-//    @POST("profile/edit-profile")
-//    fun editUserProfile(
-//        @Body requestBody: EditProfileRequest
-//    ): Call<EditProfileResponse>
-//
-//    @GET("articles/all")
-//    fun getAllArticles(): Call<List<ArticleResponse>>
-//
-//    @GET("articles/single/{id}")
-//    fun getSingleArticle(
-//        @Path("id") id: Int
-//    ): Call<ArticleResponse>
-//
-//    @GET("articles/search")
-//    fun searchArticles(
-//        @Query("keyword") keyword: String
-//    ): Call<List<ArticleResponse>>
-//
-//    @POST("articles/add-to-favorite")
-//    fun addArticleToFavorites(
-//        @Body requestBody: FavoriteRequest
-//    ): Call<ApiResponse>
-//
-//    @POST("articles/remove-from-favorite")
-//    fun removeArticleFromFavorites(
-//        @Body requestBody: FavoriteRequest
-//    ): Call<ArticleResponse>
-//
-//    @POST("articles/test")
-//    fun newRequest(): Call<ApiResponse>
+    @Multipart
+    @POST("classification/infer")
+    suspend fun uploadImage(
+        @Part file: MultipartBody.Part
+    ): FileUploadResponse
 }
 
 
